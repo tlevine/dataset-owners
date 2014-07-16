2111aee98b6126910ca0985156bb2ef904116e98
Thomas Levine <_@thomaslevine.com>
2014-07-16 05:33:51 -0400

    helper submit messages

--- messages.py
+++ messages.py
@@ -5,15 +5,17 @@ reader = csv.reader(sys.stdin)
 writer = csv.writer(sys.stdout)
 
 # Header
-writer.writerow(reader.readrow())
+writer.writerow(next(reader))
 
 for row in reader:
-    dataset, message, *_ = row
-    sys.stderr.write(dataset)
-    os.system('echo "%s" | xclip')
-    input()
-    os.system("echo '%s' | xclip" % message)
-    notes = input()
-    sent = datetime.datetime.now().isoformat()
-    newrow = dataset, message, sent, notes
-    writer.write(newrow)
+    print(row)
+    dataset, message, sent, _ = row
+    if sent == '':
+        sys.stderr.write(dataset)
+        os.system('echo "%s" | xclip')
+        input()
+        os.system("echo '%s' | xclip" % message)
+        notes = input()
+        sent = datetime.datetime.now().isoformat()
+        newrow = dataset, message, sent, notes
+        writer.writerow(newrow)
