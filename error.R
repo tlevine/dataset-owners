5f12995202cfff9c1bc0de2418d97b0d64a28d10
Thomas Levine <_@thomaslevine.com>
2014-07-18 17:49:37 -0400

    -

--- error.R
+++ error.R
@@ -1,3 +1,5 @@
+library(reshape2)
+
 m <- read.csv('data/sent-messages.csv', stringsAsFactors = FALSE)
 m$error.sending <- grepl('feedback', m$notes)
 m$deleted <- grepl('deleted', m$notes)
@@ -5,3 +7,8 @@ m$ok <- m$notes == ''
 m$page.load <- grepl('capcha', m$notes, ignore.case = TRUE) | m$notes == "The page doesn't load." | m$notes == "This one doesn't have a contact button."
 m$ok[!mapply(any, m$ok, m$deleted, m$error.sending, m$page.load)] <- TRUE
 
+m$status <- factor('', levels = c('ok', 'deleted', 'load.error', 'submit.error'))
+m$status[m$ok] <- 'ok'
+m$status[m$deleted] <- 'deleted'
+m$status[m$page.load] <- 'load.error'
+m$status[m$error.sending] <- 'submit.error'
